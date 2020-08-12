112224e3f214bb83522916b250f159a2f2502c79
shivareddy <shiva.r547@gmail.com>
2020-08-12 21:39:14 +0530

    commit files for image decorator

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,13 +1,14 @@
-Spree::Image.class_eval do
-  mount_uploader :attachment, CloudinaryUploader, :mount_on => :attachment_file_name
+module Spree
+	module ImageDecorator
+			mount_uploader :attachment, CloudinaryUploader, :mount_on => :attachment_file_name
+			def save_attached_files; end
+			def prepare_for_destroy; end
+			def destroy_attached_files; end
 
-  # Get rid of the paperclip callbacks
-  def save_attached_files; end
-  def prepare_for_destroy; end
-  def destroy_attached_files; end
-
-  def attachment_file_name
-  	# handle NilClass gracefully
-    attachment.file.nil? ? "" : attachment.file.filename
-  end
+			def attachment_file_name
+			# handle NilClass gracefully
+			attachment.file.nil? ? "" : attachment.file.filename
+			end
 end
+end
+::Spree::Image.prepend(MyStore::Spree::ImageDecorator)
