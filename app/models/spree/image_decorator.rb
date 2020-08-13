3fe6a92f6b9c683c8c9ce5152b6930e10b6f2758
shivareddy <shiva.r547@gmail.com>
2020-08-13 05:42:33 +0530

    commit files for cloudinary

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,14 +1,14 @@
 module Spree
 	module ImageDecorator
-			mount_uploader :attachment, CloudinaryUploader, :mount_on => :attachment_file_name
+			# mount_uploader :attachment, CloudinaryUploader, :mount_on => :attachment_file_name
 			def save_attached_files; end
 			def prepare_for_destroy; end
 			def destroy_attached_files; end
 
 			def attachment_file_name
 			# handle NilClass gracefully
-			attachment.file.nil? ? "" : attachment.file.filename
+			# attachment.file.nil? ? "" : attachment.file.filename
 			end
 end
 end
-::Spree::Image.prepend(MyStore::Spree::ImageDecorator)
+::Spree::Image.prepend(Spree::ImageDecorator)
